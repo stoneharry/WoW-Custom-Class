40f7de11f99b8941557d7eff820f23a1b3d08408
stoneharry <stoneharryjames@gmail.com>
2019-01-15 21:49:00 +0000

    Add SkillLineAbility DBC Util program

---
+++ SkillLineAbilityDBCUtil/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+
+namespace SkillLineAbilityDBCUtil
+{
+    class Program
+    {
+        static SkillLineAbilityDBC DBC;
+
+        static void Main(string[] args)
+        {
+            var path = "D:/WoW Resources/2019_TrinityCore/DBC Temp/SkillLineAbility.dbc";
+            var exportPath = "D:/WoW Resources/2019_TrinityCore/DBC Temp/Export/SkillLineAbility.dbc";
+            var startIndex = 0;
+            var rangeToCopy = 32;
+
+            Console.WriteLine($"Processing {path}...");
+            DBC = new SkillLineAbilityDBC(path, exportPath);
+
+            Console.WriteLine("Read entries (R) for classmask or write new data (W)?");
+            int keyread = AcceptInput();
+            if (keyread == -1)
+                return;
+            if (keyread == 0)
+            {
+                Console.WriteLine("Input classmask to read:");
+                uint classMask = ReadClassMask();
+                OutputClassMaskData(classMask);
+            }
+            else
+            {
+                Console.WriteLine("Input classmask to copy:");
+                uint readMask = ReadClassMask();
+                Console.WriteLine("Input classmask to write as instead:");
+                uint writeMask = ReadClassMask();
+
+                Console.WriteLine($"Reading mask {readMask} and writing a copy as mask {writeMask}...");
+                var records = DBC.GetAllRecords().Where(record => (((uint)record["ClassMask"]) & readMask) != 0);
+                uint id = DBC.GetAllRecords().Max(record => (uint)record["Id"]);
+                Console.WriteLine($"Read {records.Count()} records. Writing as new class mask {writeMask} starting with id {id}...");
+
+                var list = records.ToList();
+                foreach (var entry in list)
+                {
+                    entry["Id"] = ++id;
+                    entry["ClassMask"] = writeMask;
+                }
+
+                DBC.AddNewRecords(list);
+                DBC.SaveChanges();
+
+                Console.WriteLine("Done. New max records: " + DBC.GetAllRecords().Max(record => (uint)record["Id"]));
+            }
+            Console.WriteLine("Done, press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void OutputClassMaskData(uint compareMask)
+        {
+            uint count = 0;
+            foreach (var record in DBC.GetAllRecords())
+            {
+                uint classMask = (uint) record["ClassMask"];
+                if ((classMask & compareMask) != 0)
+                {
+                    Console.WriteLine($"Count found {++count} ------------------");
+                    using (var enumer = record.GetEnumerator())
+                    {
+                        do
+                        {
+                            var entry = enumer.Current;
+                            var key = entry.Key;
+                            var value = entry.Value;
+                            Console.WriteLine($"[{key}]: {value}");
+                        }
+                        while (enumer.MoveNext());
+                    }
+                }
+            }
+        }
+
+        static int AcceptInput()
+        {
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.R)
+                return 0;
+            if (key.Key == ConsoleKey.W)
+                return 1;
+            Console.WriteLine("Press R or W to read or write. Press Q to quit.");
+            if (key.Key == ConsoleKey.Q)
+                return -1;
+            return AcceptInput();
+        }
+
+        static uint ReadClassMask()
+        {
+            var line = Console.ReadLine();
+            return uint.Parse(line); // throws exception if invalid uint
+        }
+    }
+}
---
+++ SkillLineAbilityDBCUtil/SkillLineAbilityDBC.cs
@@ -0,0 +1,107 @@
+﻿using SpellEditor.Sources.DBC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Reflection;
+
+namespace SkillLineAbilityDBCUtil
+{
+    class SkillLineAbilityDBC : AbstractDBC
+    {
+        private string _exportPath;
+
+        public SkillLineAbilityDBC(string path, string exportPath)
+        {
+            _exportPath = exportPath;
+            ReadDBCFile<SkillLineAbilityStruct>(path);
+        }
+
+        public Dictionary<string, object>[] GetAllRecords()
+        {
+            return Body.RecordMaps;
+        }
+
+        public void AddNewRecords(List<Dictionary<string, object>> records)
+        {
+            var existing = Body.RecordMaps.ToList();
+            records.ToList().ForEach(record => existing.Add(record));
+            Header.RecordCount += (uint) records.Count();
+            Body.RecordMaps = existing.ToArray();
+        }
+
+        public void SaveChanges()
+        {
+            // Write spell.dbc file
+            string path = _exportPath;
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            if (File.Exists(path))
+                File.Delete(path);
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                using (BinaryWriter writer = new BinaryWriter(fileStream))
+                {
+                    int count = Marshal.SizeOf(typeof(DBCHeader));
+                    byte[] buffer = new byte[count];
+                    GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                    Marshal.StructureToPtr(Header, handle.AddrOfPinnedObject(), true);
+                    writer.Write(buffer, 0, count);
+                    handle.Free();
+
+                    for (uint i = 0; i < Header.RecordCount; ++i)
+                    {
+                        count = Marshal.SizeOf(typeof(SkillLineAbilityStruct));
+                        buffer = new byte[count];
+                        handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                        SkillLineAbilityStruct record = new SkillLineAbilityStruct();
+                        FieldInfo[] fields = record.GetType().GetFields();
+                        foreach (var f in fields)
+                        {
+                            f.SetValueForValueType(ref record, (uint)Body.RecordMaps[i][f.Name]);
+                        }
+                        record.Id = (uint)Body.RecordMaps[i]["Id"]; // delete
+                        Marshal.StructureToPtr(record, handle.AddrOfPinnedObject(), true);
+                        writer.Write(buffer, 0, count);
+                        handle.Free();
+                    }
+
+                    writer.Write(Encoding.UTF8.GetBytes("\0"));
+                }
+            }
+        }
+    }
+    
+    static class Hlp
+    {
+        public static void SetValueForValueType<T>(this FieldInfo field, ref T item, object value) where T : struct
+        {
+            field.SetValueDirect(__makeref(item), value);
+        }
+    }
+
+    [Serializable]
+    public struct SkillLineAbilityStruct
+    {
+        // These fields are used through reflection, disable warning
+#pragma warning disable 0649
+#pragma warning disable 0169
+        public uint Id;
+        public uint SkillLineDbcRecord;
+        public uint SpellDbcRecord;
+        public uint RaceMask;
+        public uint ClassMask;
+        public uint excludeRace;
+        public uint excludeClass;
+        public uint ReqSkillValue;
+        public uint SpellDbcRecordParent;
+        public uint AcquireMethod;
+        public uint TrivialSkillLineRankHigh;
+        public uint TrivialSkillLineRankLow;
+        public uint CharacterPoints1;
+        public uint CharacterPoints2;
+#pragma warning restore 0649
+#pragma warning restore 0169
+    }
+}
